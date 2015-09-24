30547bac3344818052d91a50e4d74449c969b930
ChenLong <50954265@qq.com>
2015-09-24 10:19:42 +0800

    修改主页

---
+++ src/com/remind/sevice/WeatherPostRequest.java
@@ -0,0 +1,50 @@
+package com.remind.sevice;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+public class WeatherPostRequest extends Request<String> {
+	private Map<String, String> mMap;
+	private Listener<String> mListener;
+
+	public WeatherPostRequest(String url, Listener<String> listener,
+							  ErrorListener errorListener, Map<String, String> map) {
+		super(Method.POST, url, errorListener);
+
+		mListener = listener;
+		mMap = map;
+	}
+
+	// mMap是已经按照前面的方式,设置了参数的实例
+	@Override
+	protected Map<String, String> getParams() throws AuthFailureError {
+		return mMap;
+	}
+
+	@Override
+	protected Response<String> parseNetworkResponse(NetworkResponse response) {
+		try {
+			String jsonString = new String(response.data,
+					HttpHeaderParser.parseCharset(response.headers));
+			return Response.success(new String(jsonString),
+					HttpHeaderParser.parseCacheHeaders(response));
+		} catch (UnsupportedEncodingException e) {
+			return Response.error(new ParseError(e));
+		}
+	}
+
+	@Override
+	protected void deliverResponse(String response) {
+		mListener.onResponse(response);
+	}
+
+}
