e34238994188dd8e903fc036ac2bbbbbfb903734
ChenLong <50954265@qq.com>
2016-10-18 10:45:21 +0800

    新的分支

--- gen/com/jeremyfeinstein/slidingmenu/lib/R.java
+++ gen/com/jeremyfeinstein/slidingmenu/lib/R.java
@@ -30,10 +30,10 @@ public final class R {
 		public static final int none = 0x7f0a0011;
 		public static final int right = 0x7f0a0024;
 		public static final int selected_view = 0x7f0a000a;
-		public static final int slidingmenumain = 0x7f0a010b;
+		public static final int slidingmenumain = 0x7f0a0125;
 	}
 	public static final class layout {
-		public static final int slidingmenumain = 0x7f030045;
+		public static final int slidingmenumain = 0x7f03004d;
 	}
 	public static final class styleable {
 		public static final int[] SlidingMenu = { 0x7f0100cf, 0x7f0100d0, 0x7f0100d1, 0x7f0100d2, 0x7f0100d3, 0x7f0100d4, 0x7f0100d5, 0x7f0100d6, 0x7f0100d7, 0x7f0100d8, 0x7f0100d9, 0x7f0100da, 0x7f0100db, 0x7f0100dc };
